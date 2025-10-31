10cde615b6318d9dbb9a0f530baec8c3d437afb6
salmaarayyan <salmaarayyan@gmail.com>
2025-10-31 16:58:07 +0700

    Membuat bagian header menggunakan box dengan teks "Formulir Pendaftaran"

--- app/src/main/java/com/example/formulirpendaftaran/Form.kt
+++ app/src/main/java/com/example/formulirpendaftaran/Form.kt
@@ -1,17 +1,25 @@
 package com.example.formulirpendaftaran
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FormulirPendaftaran(modifier: Modifier = Modifier) {
@@ -29,6 +37,21 @@ fun FormulirPendaftaran(modifier: Modifier = Modifier) {
             .background(Color(0xFFF6E6FF))
             .verticalScroll(rememberScrollState())
     ){
-
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(Color(0xFFB56BFF))
+                .padding(16.dp),
+            contentAlignment = Alignment.BottomStart
+        ) {
+            Text(
+                text = "Formulir Pendaftaran",
+                color = Color.White,
+                fontSize = 28.sp,
+                //fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(start = 16.dp)
+            )
+        }
     }
 }
