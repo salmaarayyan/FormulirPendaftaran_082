3fb7ac4632b619a8c98aa135b83ce1fbe73df779
salmaarayyan <salmaarayyan@gmail.com>
2025-10-31 16:53:21 +0700

    Membuat state untuk menyimpan input pengguna

--- app/src/main/java/com/example/formulirpendaftaran/Form.kt
+++ app/src/main/java/com/example/formulirpendaftaran/Form.kt
@@ -1,9 +1,16 @@
 package com.example.formulirpendaftaran
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FormulirPendaftaran(modifier: Modifier = Modifier) {
-
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
 }
