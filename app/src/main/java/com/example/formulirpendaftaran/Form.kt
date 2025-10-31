798c8c0e28e2289cf55f5624c365fb2664bd14b3
salmaarayyan <salmaarayyan@gmail.com>
2025-10-31 16:52:38 +0700

    Membuat function FormulirPendaaftaran

--- app/src/main/java/com/example/formulirpendaftaran/Form.kt
+++ app/src/main/java/com/example/formulirpendaftaran/Form.kt
@@ -1,2 +1,9 @@
 package com.example.formulirpendaftaran
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormulirPendaftaran(modifier: Modifier = Modifier) {
+
+}
