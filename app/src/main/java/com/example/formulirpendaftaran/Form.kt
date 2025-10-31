708352b3fca5e8a4f6395b6178472467145819d6
salmaarayyan <salmaarayyan@gmail.com>
2025-10-31 17:05:42 +0700

    Menambahkan spacer untuk jarak

--- app/src/main/java/com/example/formulirpendaftaran/Form.kt
+++ app/src/main/java/com/example/formulirpendaftaran/Form.kt
@@ -96,6 +96,8 @@ fun FormulirPendaftaran(modifier: Modifier = Modifier) {
                         Text(item)
                     }
                 }
+
+                Spacer(modifier = Modifier.height(16.dp))
             }
         }
 
