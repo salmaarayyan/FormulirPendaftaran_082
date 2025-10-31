934f68ad9da2915f06106d9c793661331b5f36e0
salmaarayyan <salmaarayyan@gmail.com>
2025-10-31 16:56:28 +0700

    Membuat layout utama menggunakan column agar konten tersusun vertikal

--- app/src/main/java/com/example/formulirpendaftaran/Form.kt
+++ app/src/main/java/com/example/formulirpendaftaran/Form.kt
@@ -1,11 +1,17 @@
 package com.example.formulirpendaftaran
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun FormulirPendaftaran(modifier: Modifier = Modifier) {
@@ -16,4 +22,13 @@ fun FormulirPendaftaran(modifier: Modifier = Modifier) {
 
     val jenisList = listOf("Laki-laki", "Perempuan")
     val statusList = listOf("Janda", "Lajang", "Duda")
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFFF6E6FF))
+            .verticalScroll(rememberScrollState())
+    ){
+
+    }
 }
