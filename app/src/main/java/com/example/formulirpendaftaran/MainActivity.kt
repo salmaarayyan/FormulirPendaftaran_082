d445b2c1c0061774239605051823cf3e36f7c588
salmaarayyan <salmaarayyan@gmail.com>
2025-10-31 18:16:54 +0700

    Memanggil function untuk di run

--- app/src/main/java/com/example/formulirpendaftaran/MainActivity.kt
+++ app/src/main/java/com/example/formulirpendaftaran/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             FormulirPendaftaranTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    FormulirPendaftaran(
+                    FormulirPendaftaran( // memanggil function
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
